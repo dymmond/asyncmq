8cd61907dfb87506c182dba026d69d7a8f92e710
Johannes Rueschel <chbndrhnns@users.noreply.github.com>
2025-07-06 13:21:37 +0200

    Add SessionMiddleware to default dashboard (#49)

--- asyncmq/contrib/dashboard/serve.py
+++ asyncmq/contrib/dashboard/serve.py
@@ -1,6 +1,9 @@
+import secrets
+
 from lilya.apps import Lilya
 from lilya.middleware.base import DefineMiddleware
 from lilya.middleware.cors import CORSMiddleware
+from lilya.middleware.sessions import SessionMiddleware
 from lilya.routing import Include
 
 from asyncmq.contrib.dashboard.application import dashboard
@@ -14,7 +17,11 @@ dash_app = Lilya(
             allow_methods=["*"],
             allow_headers=["*"],
             allow_credentials=True,
-        )
+        ),
+        DefineMiddleware(
+            SessionMiddleware,
+            secret_key=secrets.token_hex(32),
+        ),
     ],
 )
 
